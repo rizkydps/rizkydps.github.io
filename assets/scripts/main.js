65be8a0e85ac60579b68ce840d6f6073a9124f0f
Rizky DPS <rzkydps@gmail.com>
2021-05-27 05:14:20 +0800

    delete vendor & update

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+var icon = document.getElementById("icon");
+
+icon.onclick = function(){
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList("dark-theme")){
+    icon.src = "images/sun.png";
+  }else{
+    icon.src = "images/moon.png";
+    
+  }
+}
